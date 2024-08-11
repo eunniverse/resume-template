b1f2de22cef8cd74f0a39f13158a827a594b5f9a
eunniverse <eunisss98@gmail.com>
2024-08-12 03:39:36 +0900

    - 이메일 모달 다국어 처리(완료)

--- src/components/Detail/EmailModal.tsx
+++ src/components/Detail/EmailModal.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {useRef} from "react";
 import emailjs from '@emailjs/browser';
 import ReactDOM from "react-dom";
+import {useTranslations} from "next-intl";
 
 type Props = {
     show: boolean,
@@ -12,6 +13,7 @@ type Props = {
 
 export const EmailModal = (props: Props) => {
     const form = useRef<HTMLFormElement>(null);
+    const t = useTranslations('common.emailModal');
 
     const onSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -26,13 +28,13 @@ export const EmailModal = (props: Props) => {
                     process.env.NEXT_PUBLIC_MAIL_PUBLIC_KEY as string
                 )
                 .then(() => {
-                    alert('연락주셔서 감사합니다! 빠른 시간 내에 답변드리겠습니다!');
+                    alert(t('finishedSend'));
                     props.onClose();
 
                 });
         } catch (error:any) {
             console.log(error.text);
-            alert('메시지 전송이 실패했습니다.\neunisss98@gmail.com 으로 연락 부탁드립니다!');
+            alert(t('errorSend'));
         }
     };
 
@@ -48,7 +50,7 @@ export const EmailModal = (props: Props) => {
                     <div className="modal-header w-full flex-container">
                         <span className='h-full w-full pl-2 text-left'>
                             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                                메일전송
+                                {t('modalTitle')}
                             </h3>
                         </span>
                         <span className='h-full'>
@@ -61,24 +63,24 @@ export const EmailModal = (props: Props) => {
                         <form ref={form} className="p-4 md:p-5" onSubmit={onSubmitForm}>
                             <div className="grid gap-4 mb-4 grid-cols-2">
                                 <div className="col-span-2">
-                                    <label htmlFor="from_corporate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">기업명</label>
-                                    <input type="text" name="from_corporate" id="from_corporate" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="기업명" required/>
+                                    <label htmlFor="from_corporate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('company')}</label>
+                                    <input type="text" name="from_corporate" id="from_corporate" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={t('company')} required/>
                                 </div>
                                 <div className="col-span-2">
-                                    <label htmlFor="from_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">성함</label>
-                                    <input type="text" name="from_name" id="from_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="성함" required/>
+                                    <label htmlFor="from_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('name')}</label>
+                                    <input type="text" name="from_name" id="from_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={t('name')} required/>
                                 </div>
                                 <div className="col-span-2">
-                                    <label htmlFor="from_email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
-                                    <input type="text" name="from_email" id="from_email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="이메일" required/>
+                                    <label htmlFor="from_email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('email')}</label>
+                                    <input type="text" name="from_email" id="from_email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={t('email')} required/>
                                 </div>
                                 <div className="col-span-2">
-                                    <label htmlFor="from_tel" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">전화번호</label>
-                                    <input type="text" name="from_tel" id="from_tel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="전화번호"/>
+                                    <label htmlFor="from_tel" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('mobile')}</label>
+                                    <input type="text" name="from_tel" id="from_tel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={t('mobile')}/>
                                 </div>
                                 <div className="col-span-2">
-                                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">메시지</label>
-                                    <input type="text" name="message" id="message" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="메시지" required/>
+                                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('message')}</label>
+                                    <input type="text" name="message" id="message" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={t('message')} required/>
                                 </div>
                             </div>
 
